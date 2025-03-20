b465382179ddc483286e083f48d2174b573cef55
NewtlyCroco <willmitchellbusinessva@gmail.com>
2025-03-20 15:14:22 +0200

    UI Refactor!

--- app/src/main/java/com/example/leafhunterdevelopment/ui/dashboard/CameraViewModel.kt
+++ app/src/main/java/com/example/leafhunterdevelopment/ui/dashboard/CameraViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.leafhunterdevelopment.ui.notifications
+package com.example.leafhunterdevelopment.ui.dashboard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class CameraViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
